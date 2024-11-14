6c77ab371b8fed209a261766fb2412d19b0e7108
Mixces <aar.hcms@gmail.com>
2024-11-14 02:04:02 -0600

    remove debug menu restriction

--- src/main/java/me/mixces/statuseffects/mixin/GameRendererMixin.java
+++ src/main/java/me/mixces/statuseffects/mixin/GameRendererMixin.java
@@ -1,19 +1,14 @@
 package me.mixces.statuseffects.mixin;
 
 import me.mixces.statuseffects.StatusEffectsHUD;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-
-	@Shadow
-	private Minecraft minecraft;
 
 	@Inject(
 		method = "render(FJ)V",
@@ -24,9 +19,7 @@ public class GameRendererMixin {
 		)
 	)
 	private void statusEffects$drawStatusEffects(float tickDelta, long startTime, CallbackInfo ci) {
-		if (!minecraft.options.debugEnabled) {
-			StatusEffectsHUD hud = new StatusEffectsHUD();
-			hud.drawStatusEffects();
-		}
+		StatusEffectsHUD hud = new StatusEffectsHUD();
+		hud.drawStatusEffects();
 	}
 }
